b58e1a388304663e7031e973606a5e4481ab9769
Andre <sarreqiandrea@gmail.com>
2023-01-27 18:50:10 +0000

    Adding a question meter,timer and question shuffle to the quiz

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,13 +6,14 @@ const progressBarFull = document.querySelector('#progressBarFull');
 const SCORE_POINTS = 10;
 const MAX_QUESTIONS = 10;
 
-const currentQuestion = {};
-const acceptinAnswers = true;
+let currentQuestion = {};
+let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [{
+let questions = [
+    {
     question: 'In which U.S city was the hip hop culture created?',
     choice1: 'Chicago',
     choice2: 'Los Angeles',
@@ -62,12 +63,12 @@ let questions = [{
     answer: 'Reasonable Doubt',
 },
 {
-    question: 'Which of the following did NOT die in battle in Mesopotamia?',
-    choice1: 'Gordian III',
-    choice2: 'Julius',
-    choice3: 'Crassus',
-    choice4: 'Valerian',
-    answer: 'Valerian'
+    question: 'The UK number 1 hit “Ghetto Gospel” was a collaboration between Elton John and which hip hop legend?',
+    choice1: 'Lil Wayne',
+    choice2: 'Tupac Shakur',
+    choice3: 'Eminem',
+    choice4: 'LL COOL J',
+    answer: 'Tupac Shakur'
 },
 {
     question: 'What was the first rap name of Biggie Smalls, also known as The Notorious B.I.G?',
@@ -86,3 +87,134 @@ let questions = [{
     answer: 'Death Row Records',
 },
 ];
+
+ document.addEventListener("DOMContentLoaded", function () {
+    /*
+     *Display timer countdown to the player.
+     *Reset score on time out to the result page.
+     */
+    var second = 60;
+    var timeInterval = setInterval(quizTimer, 1000);
+
+    function quizTimer() {
+        document.getElementById('timer').innerHTML = second + "s left";
+        second--;
+        if (second === -2) {
+            clearInterval(timeInterval);
+            document.getElementById('timer').innerHTML = "0s left";
+            alert('Your time has run out!');
+            resetScore();
+            return window.location.assign('result.html');
+        }
+    }
+
+    /** 
+     * Start the game by setting the default value to the score counter and progress bar 
+     * and calling the first question along with its answers.
+     * The questions are randomized.
+     */
+    function startGame() {
+        questionCounter = 0;
+        score = 0;
+        availableQuestions = [...questions]; /* Spread operator */
+        getNewQuestion();
+    }
+
+    //Randomize question choices
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    /*
+     *Get new question from questions array and display in the front end.
+     *Increment question counter and progress bar values.
+     *Shuffle answer choices.
+     *Display the randomized choices in the front end.
+     */
+    getNewQuestion = () => {
+        if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+            localStorage.setItem('recentScore', score);
+
+            return window.location.assign('result.html'); /* Take player to end game*/
+        }
+
+        questionCounter++;
+        progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+        progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
+
+
+        const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+        currentQuestion = availableQuestions[questionsIndex];
+        question.innerText = currentQuestion.question;
+
+        const answerChoices = [
+            currentQuestion.choice1,
+            currentQuestion.choice2,
+            currentQuestion.choice3,
+            currentQuestion.choice4,
+        ];
+
+        shuffle(answerChoices);
+
+        choices.forEach((choice, index) => {
+            choice.innerHTML = answerChoices[index];
+        });
+
+        availableQuestions.splice(questionsIndex, 1);
+
+        acceptingAnswers = true;
+    };
+
+    /*
+     *Detect user click
+     *Compare user's selected choice with the correct answer value.
+     *Light up green or yellow depending on correctness and if correct, increment score value.
+     *Remove the light up effect.
+     */
+    choices.forEach(choice => {
+        choice.addEventListener('click', e => {
+            if (!acceptingAnswers) return;
+
+            acceptingAnswers = false;
+            const selectedChoice = e.target;
+            const selectedAnswer = selectedChoice.dataset.number;
+            let classToApply = selectedChoice.innerText == currentQuestion.correctAnswer ? 'correct' : 'incorrect';
+
+            if (classToApply === 'correct') {
+                incrementScore(SCORE_POINTS);
+            }
+
+            selectedChoice.parentElement.classList.add(classToApply);
+
+            setTimeout(() => {
+                selectedChoice.parentElement.classList.remove(classToApply);
+                getNewQuestion();
+            }, 1000);
+        });
+    });
+
+    /*
+     *Increase score value.
+     *Display the new value in the front end.
+     */
+    incrementScore = num => {
+        score += num;
+        scoreText.innerText = score;
+    };
+
+    /*
+     *Reset displayed score value to 0.
+     *Reset score value to 0 in the local storage.
+     */
+    function resetScore() {
+        score = 0;
+        scoreText.innerText = 0;
+        localStorage.setItem('recentScore', score);
+    }
+
+    startGame();
+
+});
